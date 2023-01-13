5c7f80f41d34ae92da5428e5263e48e31d33b890
tdurant <tahiradurant47@gmail.com>
2023-01-12 23:21:50 -0500

    Fixed edit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 // console.log("hello world")
 
-const button=document.createElement("Button")
-button.innerText="Submit"
-button.addEventListener("click,", () =>{   
+
+class love {
+    constructor(love) {
+      this.state = {
+        fname:'',
+        sname:'',
+       Percentage: '',
+       Result:''
+    }
+  }
 }
-)
 function Submit(){
+    if(this.state.fname ==""|| this.state.sname== ""){
+    }
 
 }
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -20,5 +29,33 @@ fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=Jo
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+    
+window.onload = (event) =>{
+    document.querySelector('form').addEventListener('submit', (event) => {
+
+        const userInput = document.querySelector('input type="text1"]').value
+        const userInput1= document.querySelector('input type="text2"]').value
+        fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John' + userInput).then(response => {
+            return response.json()
+        }).then(
+            json => {
+                document.querySelector('# Your Name').innerText = data.YourName
+                document.querySelector('#Crushes Name ').innerText = data.CrushesName
+                document.querySelector('#Percentage').innerText = data.Percentage
+                },
+            err => console.log(err)
+        )
+    })
+}
 
 
+function Compatibility(){
+const YourName= document.getElementById("User input").value
+const CrushesName= document.getElementById("userInput1").value
+
+
+window.onload= function(){
+const button=document.createElement("Submit")
+button.innerText="Submit"
+button.addEventListener("click", Submit)};
+}
