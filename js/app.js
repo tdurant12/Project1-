d08b029503adb968ef0280425e737a0b52e02a39
tdurant <tahiradurant47@gmail.com>
2023-01-13 17:21:15 -0500

    Entered Place Holder

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 // console.log("hello world")
 
+const button = document.getElementById('button-id');
 
-class love {
-    constructor(love) {
-      this.state = {
-        fname:'',
-        sname:'',
-       Percentage: '',
-       Result:''
-    }
-  }
-}
-function Submit(){
-    if(this.state.fname ==""|| this.state.sname== ""){
-    }
-
-}
-
+if (button){
+    button.addEventListener('click', function () {
+        alert( "RESULTS LOADING..."); 
+})};
+ 
 const options = {
 	method: 'GET',
 	headers: {
@@ -28,34 +18,4 @@ const options = {
 fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
-    
-window.onload = (event) =>{
-    document.querySelector('form').addEventListener('submit', (event) => {
-
-        const userInput = document.querySelector('input type="text1"]').value
-        const userInput1= document.querySelector('input type="text2"]').value
-        fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John' + userInput).then(response => {
-            return response.json()
-        }).then(
-            json => {
-                document.querySelector('# Your Name').innerText = data.YourName
-                document.querySelector('#Crushes Name ').innerText = data.CrushesName
-                document.querySelector('#Percentage').innerText = data.Percentage
-                },
-            err => console.log(err)
-        )
-    })
-}
-
-
-function Compatibility(){
-const YourName= document.getElementById("User input").value
-const CrushesName= document.getElementById("userInput1").value
-
-
-window.onload= function(){
-const button=document.createElement("Submit")
-button.innerText="Submit"
-button.addEventListener("click", Submit)};
-}
+	.catch(err => console.error(err))
