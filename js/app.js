360b2de664f4ef5bf5ef2c9611e0a8b2adf3ba58
tdurant <tahiradurant47@gmail.com>
2023-01-17 14:19:05 -0500

    functionality fix

--- js/app.js
+++ js/app.js
@@ -1,20 +1,52 @@
-// console.log("hello world")
+const myURL= 'https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John'
+function prompt(){
 
-// const submitButton = document.querySelector('.submitButton');
-const yourName= document.querySelector('.fname')
-// const yourLover=document.querySelector('.sname')
-
-//CALL API//
-
+	alert("You have a "+ "" + getPercentage+ "%"+ ""+ " Chance at Love!")
+}
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '1f01566881msh9a32412bfaee0d9p19eac8jsnaaa4bb686b9a',
+		'X-RapidAPI-Key': '9eaadbf430msh0c6ea8a45eeb30bp13fff5jsnab6ea5c9720e',
 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
 	}
 };
 
+
 fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err))
+	.catch(err => console.error(err));{
+	
+		
+	}
+	
+
+
+
+const lover= document.getElementById('lover');
+
+lover.addEventListener('submit',function(e){
+	e.preventDefault();
+
+
+const loveform =new FormData(lover);
+
+const getR= document.getElementById("Get-results")
+
+
+console.log(([...loveform]),getPercentage +"%")
+
+})
+
+let getPercentage= Math.random() *100
+getPercentage= Math.floor(getPercentage) +1
+if(getPercentage >80){
+	
+	}
+if(getPercentage >40 && getPercentage <=80){
+
+}
+if(getPercentage <=40){
+}
+if(getPercentage >20 && getPercentage <=40){
+}
